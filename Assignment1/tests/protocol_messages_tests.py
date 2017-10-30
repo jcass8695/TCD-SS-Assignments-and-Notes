b32d1069d942bafe4aa2fd227496e30187010d1d
Jack Cassidy <j.cassidy45@gmail.com>
2017-10-30 16:11:09 +0000

    Add check_kill unit test to validate KILL_SERVICE commands

--- Assignment1/tests/protocol_messages_tests.py
+++ Assignment1/tests/protocol_messages_tests.py
@@ -2,16 +2,24 @@ import unittest
 import sys
 sys.path.append('../src')
 
-from protocol_messages import check_hello
+from protocol_messages import check_hello, check_kill
 
 class TestValidations(unittest.TestCase):
-    def test_helo(self):
-        self.assertTrue(check_hello('HELO'))
-        self.assertTrue(check_hello('HELO BASE_TEST'))
-        self.assertTrue(check_hello('HELO BASE TEST'))
-        self.assertFalse(check_hello('HELLO'))
-        self.assertTrue(check_hello('HELOBASE_TEST'))
+    def test_hello(self):
+        self.assertTrue(check_hello('HELO\n'))
+        self.assertTrue(check_hello('HELO BASE_TEST\n'))
+        self.assertTrue(check_hello('HELO BASE TEST\n'))
 
+        self.assertFalse(check_hello('HELO BASE_TEST'))
+        self.assertFalse(check_hello('HELLO\n'))
+        self.assertFalse(check_hello('HELOBASE_TEST\n'))
+
+    def test_kill(self):
+        self.assertTrue(check_kill('KILL_SERVICE\n'))
+        
+        self.assertFalse(check_kill('KILL_SERVICE'))
+        self.assertFalse(check_kill('KILL SERVICE\n'))
+        self.assertFalse(check_kill('kill_service\n'))
 
 if __name__ == '__main__':
     unittest.main()
