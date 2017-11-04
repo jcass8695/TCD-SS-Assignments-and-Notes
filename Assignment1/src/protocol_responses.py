78913ec7fe23cf94cd3ee3cb39033fef98b7bb75
Jack Cassidy <j.cassidy45@gmail.com>
2017-11-04 15:58:46 +0000

    Output all of the servers responses

--- Assignment1/src/protocol_responses.py
+++ Assignment1/src/protocol_responses.py
@@ -22,26 +22,35 @@ def respond_to_hello(message, ip, port="3000"):
     # Check if there's extra text beside the 'HELO'
     if len(message.split(' ', maxsplit=1)) > 1:
         extra_text = ' ' + message.split(' ', maxsplit=1)[1].strip()
-        return str(RESPONSE_TABLE.get(pr_msg.HELLO)).format(extra_text, ip, port).encode()
+        response = str(RESPONSE_TABLE.get(pr_msg.HELLO)).format(extra_text, ip, port).encode()
     else:
-        return str(RESPONSE_TABLE.get(pr_msg.HELLO)).format('', ip, port).encode()
+        response = str(RESPONSE_TABLE.get(pr_msg.HELLO)).format('', ip, port).encode()
+
+    print("Server responded with:\n{}".format(response.decode()))
+    return response
 
 
 def respond_to_join(chatroom_name, room_id, join_id, ip, port=3000):
-    return str(RESPONSE_TABLE.get(pr_msg.JOIN)).format(chatroom_name, ip, port, room_id, join_id).encode()
-
+    response = str(RESPONSE_TABLE.get(pr_msg.JOIN)).format(chatroom_name, ip, port, room_id, join_id).encode()
+    print("Server responded with:\n{}".format(response.decode()))
+    return response
 
 def respond_to_leave(room_id, join_id):
-    return str(RESPONSE_TABLE.get(pr_msg.LEAVE)).format(room_id, join_id).encode()
-
+    response = str(RESPONSE_TABLE.get(pr_msg.LEAVE)).format(room_id, join_id).encode()
+    print("Server responded with:\n{}".format(response.decode()))
+    return response
 
 def respond_to_message(room_id, client_name, message):
-    return str(RESPONSE_TABLE.get(pr_msg.MESSAGE)).format(room_id, client_name, message).encode()
-
+    response = str(RESPONSE_TABLE.get(pr_msg.MESSAGE)).format(room_id, client_name, message).encode()
+    print("Server responded with:\n{}".format(response.decode()))
+    return response
 
 def respond_with_error(error_id):
     if error_id is 1:
-        return str(RESPONSE_TABLE.get(pr_msg.ERROR1)).encode()
+        response = str(RESPONSE_TABLE.get(pr_msg.ERROR1)).encode()
 
     elif error_id is 2:
-        return str(RESPONSE_TABLE.get(pr_msg.ERROR2)).encode()
+        response = str(RESPONSE_TABLE.get(pr_msg.ERROR2)).encode()
+
+    print("Server responded with:\n{}".format(response.decode()))
+    return response
