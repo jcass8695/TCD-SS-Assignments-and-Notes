712d37ef56e954329fcd1c0c042c9003e6eb2cd2
Jack Cassidy <j.cassidy45@gmail.com>
2017-11-05 14:59:44 +0000

    Add check_dsiconnect UT

--- Assignment1/tests/protocol_messages_tests.py
+++ Assignment1/tests/protocol_messages_tests.py
@@ -2,7 +2,7 @@ import unittest
 import sys
 sys.path.append('../src')
 
-from protocol_messages import check_hello, check_kill, check_join, check_leave, check_message, parse_join, parse_leave, parse_message
+from protocol_messages import check_hello, check_kill, check_join, check_leave, check_disconnect, check_message, parse_join, parse_leave, parse_message
 
 
 class TestValidations(unittest.TestCase):
@@ -23,120 +23,98 @@ class TestValidations(unittest.TestCase):
         self.assertFalse(check_kill('kill_service\n'))
 
     def test_join(self):
-        self.assertTrue(check_join(
-            "JOIN_CHATROOM: chatroom_1\nCLIENT_IP: 0\nPORT: 0\nCLIENT_NAME: client_1\n"))
+        self.assertTrue(check_join("JOIN_CHATROOM: chatroom_1\nCLIENT_IP: 0\nPORT: 0\nCLIENT_NAME: client_1\n"))
 
-        self.assertTrue(check_join(
-            "JOIN_CHATROOM: chatroom 1\nCLIENT_IP: 0\nPORT: 0\nCLIENT_NAME: client 1\n"))
+        self.assertTrue(check_join("JOIN_CHATROOM: chatroom 1\nCLIENT_IP: 0\nPORT: 0\nCLIENT_NAME: client 1\n"))
 
-        self.assertTrue(check_join(
-            "JOIN_CHATROOM: chatroom 1 1\nCLIENT_IP: 0\nPORT: 0\nCLIENT_NAME: mad max 21\n"))
+        self.assertTrue(check_join("JOIN_CHATROOM: chatroom 1 1\nCLIENT_IP: 0\nPORT: 0\nCLIENT_NAME: mad max 21\n"))
 
-        self.assertTrue(check_join(
-            "JOIN_CHATROOM: chatroom_1\nCLIENT_IP: 0\nPORT: 0\nCLIENT_NAME: mad_max_21\n"))
+        self.assertTrue(check_join("JOIN_CHATROOM: chatroom_1\nCLIENT_IP: 0\nPORT: 0\nCLIENT_NAME: mad_max_21\n"))
 
-        self.assertFalse(check_join(
-            "JOIN_CHATROOM: \nCLIENT_IP: 0\nPORT: 0\nCLIENT_NAME: client_1\n"))
+        self.assertFalse(check_join("JOIN_CHATROOM: \nCLIENT_IP: 0\nPORT: 0\nCLIENT_NAME: client_1\n"))
 
-        self.assertFalse(check_join(
-            "JOIN_CHATROOM: chatroom_1\nCLIENT_IP: 0\nPORT: 0\nCLIENT_NAME: \n"))
+        self.assertFalse(check_join("JOIN_CHATROOM: chatroom_1\nCLIENT_IP: 0\nPORT: 0\nCLIENT_NAME: \n"))
 
-        self.assertFalse(check_join(
-            "JOIN_CHATROOM:chatroom_1\nCLIENT_IP: 0\nPORT: 0\nCLIENT_NAME: client_1\n"))
+        self.assertFalse(check_join("JOIN_CHATROOM:chatroom_1\nCLIENT_IP: 0\nPORT: 0\nCLIENT_NAME: client_1\n"))
 
-        self.assertFalse(check_join(
-            "JOIN_CHATROOM: chatroom_1\nCLIENT_IP: 0\nPORT: 0\nCLIENT_NAME:client_1\n"))
+        self.assertFalse(check_join("JOIN_CHATROOM: chatroom_1\nCLIENT_IP: 0\nPORT: 0\nCLIENT_NAME:client_1\n"))
 
-        self.assertFalse(check_join(
-            "JOIN_CHATROOM: chatroom_1\nCLIENT_IP:0\nPORT: 0\nCLIENT_NAME: client_1\n"))
+        self.assertFalse(check_join("JOIN_CHATROOM: chatroom_1\nCLIENT_IP:0\nPORT: 0\nCLIENT_NAME: client_1\n"))
 
-        self.assertFalse(check_join(
-            "JOIN_CHATROOM: chatroom_1\nCLIENT_IP: 0\nPORT:0\nCLIENT_NAME: client_1\n"))
+        self.assertFalse(check_join("JOIN_CHATROOM: chatroom_1\nCLIENT_IP: 0\nPORT:0\nCLIENT_NAME: client_1\n"))
 
     def test_leave(self):
-        self.assertTrue(check_leave(
-            "LEAVE_CHATROOM: 123\nJOIN_ID: 456\nCLIENT_NAME: client_1\n"))
+        self.assertTrue(check_leave("LEAVE_CHATROOM: 123\nJOIN_ID: 456\nCLIENT_NAME: client_1\n"))
 
-        self.assertTrue(check_leave(
-            "LEAVE_CHATROOM: 123\nJOIN_ID: 456\nCLIENT_NAME: client 1\n"))
+        self.assertTrue(check_leave("LEAVE_CHATROOM: 123\nJOIN_ID: 456\nCLIENT_NAME: client 1\n"))
 
-        self.assertTrue(check_leave(
-            "LEAVE_CHATROOM: 123\nJOIN_ID: 456\nCLIENT_NAME: mad max 21\n"))
+        self.assertTrue(check_leave("LEAVE_CHATROOM: 123\nJOIN_ID: 456\nCLIENT_NAME: mad max 21\n"))
 
-        self.assertTrue(check_leave(
-            "LEAVE_CHATROOM: 123\nJOIN_ID: 456\nCLIENT_NAME: mad_max_21\n"))
+        self.assertTrue(check_leave("LEAVE_CHATROOM: 123\nJOIN_ID: 456\nCLIENT_NAME: mad_max_21\n"))
 
-        self.assertFalse(check_leave(
-            "LEAVE_CHATROOM: room_1\nJOIN_ID: 456\nCLIENT_NAME: client_1\n"))
+        self.assertFalse(check_leave("LEAVE_CHATROOM: room_1\nJOIN_ID: 456\nCLIENT_NAME: client_1\n"))
 
-        self.assertFalse(check_leave(
-            "LEAVE_CHATROOM: 123\nJOIN_ID: fourfivesix\nCLIENT_NAME: client_1\n"))
+        self.assertFalse(check_leave("LEAVE_CHATROOM: 123\nJOIN_ID: fourfivesix\nCLIENT_NAME: client_1\n"))
+
+    def test_disconnect(self):
+        self.assertTrue(check_disconnect("DISCONNECT: 0\nPORT: 0\nCLIENT_NAME: client_1\n"))
+        self.assertTrue(check_disconnect("DISCONNECT: 0\nPORT: 0\nCLIENT_NAME: client 1\n"))
+        self.assertTrue(check_disconnect("DISCONNECT: 0\nPORT: 0\nCLIENT_NAME: mad max 21\n"))
+        self.assertTrue(check_disconnect("DISCONNECT: 0\nPORT: 0\nCLIENT_NAME: mad_max_21\n"))
+        self.assertFalse(check_disconnect("DISCONNECT: 10\nPORT: 0\nCLIENT_NAME: client_1\n"))
+        self.assertFalse(check_disconnect("DISCONNECT: 0\nPORT: 10\nCLIENT_NAME: client_1\n"))
+        self.assertFalse(check_disconnect("DISCONNECT: 0\nPORT: 0\nCLIENT_NAME: \n"))
+        self.assertFalse(check_disconnect("DISCONNECT: 0\nPORT: 0\nCLIENT_NAME:client_1\n"))
+
+
 
     def test_message(self):
-        self.assertTrue(check_message(
-            "CHAT: 21\nJOIN_ID: 123\nCLIENT_NAME: jack\nMESSAGE: This is a message\n\n"))
+        self.assertTrue(check_message("CHAT: 21\nJOIN_ID: 123\nCLIENT_NAME: jack\nMESSAGE: This is a message\n\n"))
 
-        self.assertTrue(check_message(
-            "CHAT: 21\nJOIN_ID: 123\nCLIENT_NAME: jack\nMESSAGE: This is a message! Wow with punctuation?\n\n"))
+        self.assertTrue(check_message("CHAT: 21\nJOIN_ID: 123\nCLIENT_NAME: jack\nMESSAGE: This is a message! Wow with punctuation?\n\n"))
 
-        self.assertTrue(check_message(
-            "CHAT: 21\nJOIN_ID: 123\nCLIENT_NAME: jack\nMESSAGE: Thisisamessagewithnospaces\n\n"))
+        self.assertTrue(check_message("CHAT: 21\nJOIN_ID: 123\nCLIENT_NAME: jack\nMESSAGE: Thisisamessagewithnospaces\n\n"))
 
-        self.assertTrue(check_message(
-            "CHAT: 21\nJOIN_ID: 123\nCLIENT_NAME: jack\nMESSAGE: This is a message\nOn multiple lines\n\n"))
+        self.assertTrue(check_message("CHAT: 21\nJOIN_ID: 123\nCLIENT_NAME: jack\nMESSAGE: This is a message\nOn multiple lines\n\n"))
 
-        self.assertFalse(check_message(
-            "CHAT: chatroom1\nJOIN_ID: 123\nCLIENT_NAME: jack\nMESSAGE: This is a message\n\n"))
+        self.assertFalse(check_message("CHAT: chatroom1\nJOIN_ID: 123\nCLIENT_NAME: jack\nMESSAGE: This is a message\n\n"))
 
-        self.assertFalse(check_message(
-            "CHAT: 21\nJOIN_ID: player1\nCLIENT_NAME: jack\nMESSAGE: This is a message\n\n"))
+        self.assertFalse(check_message("CHAT: 21\nJOIN_ID: player1\nCLIENT_NAME: jack\nMESSAGE: This is a message\n\n"))
 
-        self.assertFalse(check_message(
-            "CHAT: 21\nJOIN_ID: 123\nCLIENT_NAME: \nMESSAGE: This is a message\n\n"))
+        self.assertFalse(check_message("CHAT: 21\nJOIN_ID: 123\nCLIENT_NAME: \nMESSAGE: This is a message\n\n"))
 
-        self.assertFalse(check_message(
-            "CHAT: 21\nJOIN_ID: \nCLIENT_NAME: jack\nMESSAGE: This is a message\n\n"))
+        self.assertFalse(check_message("CHAT: 21\nJOIN_ID: \nCLIENT_NAME: jack\nMESSAGE: This is a message\n\n"))
 
-        self.assertFalse(check_message(
-            "CHAT: 21\nJOIN_ID: 123\nCLIENT_NAME: \nMESSAGE: This is a message\n\n"))
+        self.assertFalse(check_message("CHAT: 21\nJOIN_ID: 123\nCLIENT_NAME: \nMESSAGE: This is a message\n\n"))
 
-        self.assertFalse(check_message(
-            "CHAT: 21\nJOIN_ID: 123\nCLIENT_NAME: jack\nMESSAGE: This is a message\n"))
+        self.assertFalse(check_message("CHAT: 21\nJOIN_ID: 123\nCLIENT_NAME: jack\nMESSAGE: This is a message\n"))
 
-        self.assertFalse(check_message(
-            "CHAT: 21\nJOIN_ID: \nCLIENT_NAME: jackMESSAGE: This is a message\n\n"))
+        self.assertFalse(check_message("CHAT: 21\nJOIN_ID: \nCLIENT_NAME: jackMESSAGE: This is a message\n\n"))
 
     def test_parse_join(self):
-        chatroom_name, client_name = parse_join(
-            "JOIN_CHATROOM: chatroom_1\nCLIENT_IP: 0\nPORT: 0\nCLIENT_NAME: client_1\n")
+        chatroom_name, client_name = parse_join("JOIN_CHATROOM: chatroom_1\nCLIENT_IP: 0\nPORT: 0\nCLIENT_NAME: client_1\n")
         self.assertEqual(chatroom_name, "chatroom_1")
         self.assertEqual(client_name, "client_1")
 
-        chatroom_name, client_name = parse_join(
-            "JOIN_CHATROOM: chatroom 1\nCLIENT_IP: 0\nPORT: 0\nCLIENT_NAME: client 1\n")
+        chatroom_name, client_name = parse_join("JOIN_CHATROOM: chatroom 1\nCLIENT_IP: 0\nPORT: 0\nCLIENT_NAME: client 1\n")
         self.assertEqual(chatroom_name, "chatroom 1")
         self.assertEqual(client_name, "client 1")
 
     def test_parse_leave(self):
-        room_id, join_id = parse_leave(
-            "LEAVE_CHATROOM: 543\nJOIN_ID: 211\nCLIENT_NAME: client_1\n")
+        room_id, join_id = parse_leave("LEAVE_CHATROOM: 543\nJOIN_ID: 211\nCLIENT_NAME: client_1\n")
         self.assertEqual(room_id, 543)
         self.assertEqual(join_id, 211)
 
     def test_parse_message(self):
-        room_id, join_id, message_text = parse_message(
-            "CHAT: 122\nJOIN_ID: 32\nCLIENT_NAME: client_1\nMESSAGE: Hey, how's it going?\n\n")
+        room_id, join_id, message_text = parse_message("CHAT: 122\nJOIN_ID: 32\nCLIENT_NAME: client_1\nMESSAGE: Hey, how's it going?\n\n")
         self.assertEqual(room_id, 122)
         self.assertEqual(join_id, 32)
         self.assertEqual(message_text, "Hey, how's it going?")
 
-        room_id, join_id, message_text = parse_message(
-            "CHAT: 122\nJOIN_ID: 32\nCLIENT_NAME: client_1\nMESSAGE: Hey, how's it going?\nThis is a test message..?\n\n")
-
+        room_id, join_id, message_text = parse_message("CHAT: 122\nJOIN_ID: 32\nCLIENT_NAME: client_1\nMESSAGE: Hey, how's it going?\nThis is a test message..?\n\n")
         self.assertEqual(room_id, 122)
         self.assertEqual(join_id, 32)
-        self.assertEqual(
-            message_text, "Hey, how's it going?\nThis is a test message..?")
+        self.assertEqual(message_text, "Hey, how's it going?\nThis is a test message..?")
 
 
 if __name__ == '__main__':
