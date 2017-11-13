e5cc083436c1c8031143df6bc19d4b89e54fac08
Jack Cassidy <j.cassidy45@gmail.com>
2017-11-13 15:13:05 +0000

    Refactor run script

--- Assignment1/run.sh
+++ Assignment1/run.sh
@@ -2,10 +2,13 @@
 
 echo "Enter the IP the servers should listen on, defaults to 0.0.0.0"
 read IP
-
-if [[ -z "$IP" ]]; then
-    echo "Default"
-    python3 src/server.py 0.0.0.0
+if [[ $# == 1 ]]; then
+    if [[ -z "$1" ]]; then
+        echo "Default"
+        python3 src/server.py 0.0.0.0
+    else
+        python3 src/server.py $1
+    fi
 else
-    python3 src/server.py $IP
+    echo "Expecting 1 argument only, the IP of the server"
 fi
