5d9ecb4cd301f4a3c544733fc8a63e87b89607ef
Jack Cassidy <j.cassidy45@gmail.com>
2017-11-13 15:21:39 +0000

    Removing old code from run script

--- Assignment1/run.sh
+++ Assignment1/run.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-echo "Enter the IP the servers should listen on, defaults to 0.0.0.0"
-read IP
-if [[ $# == 1 ]]; then
+if (( $# <= 1 )); then
     if [[ -z "$1" ]]; then
-        echo "Default"
         python3 src/server.py 0.0.0.0
     else
         python3 src/server.py $1
     fi
 else
-    echo "Expecting 1 argument only, the IP of the server"
+    echo "Expecting 1 argument, the IP of the server or nothing"
 fi
