bc3690f2be7233038a7159f76e015eb64b67fa49
Jack Cassidy <j.cassidy45@gmail.com>
2017-11-20 20:46:31 +0000

    Server receives PUT requests and updates file

--- src/node.py
+++ src/node.py
@@ -1,18 +1,24 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
-import json
 
 app = Flask(__name__)
 api = Api(app)
 
+
 class FileServer(Resource):
     def get(self):
-        with open('test.txt', 'r') as file:
-            file_str = file.read()
-        
+        with open('src/test.txt', 'r') as in_file:
+            file_str = in_file.read()
         return file_str
+
+    def put(self):
+        new_text = request.form['data']
+        with open('src/test.txt', 'w') as out_file:
+            print(out_file.write(new_text + '\n'))
+        return 'write successful'
+
 
 api.add_resource(FileServer, '/')
 
 if __name__ == '__main__':
-    app.run(debug=True)      
+    app.run(debug=True)
