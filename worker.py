2e88595294161ba09accf2e77c7e8736443ceb21
Jack Cassidy <j.cassidy45@gmail.com>
2017-11-29 15:48:50 +0000

    Refactor worker

--- worker.py
+++ worker.py
@@ -8,49 +8,11 @@ from radon.cli.harvest import CCHarvester
 from radon.cli import Config
 
 
-def work():
+class Worker:
     files_list_url = 'https://api.github.com/repos/JCass45/CS4400-Internet-Applications-Chat-Server/git/trees/{}'
-    with open('github-token', 'r') as f:
-        token = f.read().split()[0]
-
-    blob_urls = {}
-    files = []
-    cc = 0
-    sha = requests.get('http://127.0.0.1:5000/').json()['sha']
-    payload = {
-        'recursive': 'true',
-        'access_token': token
-    }
-
-    # Get file tree for given commit SHA
-    resp = requests.get(files_list_url.format(sha), params=payload)
-    if resp.status_code == 301:
-        redirect_url = resp.headers['location'].split('?')[0]
-        payload = {'access_token': token}
-        resp = requests.get(redirect_url, params=payload)
-
-    file_tree = resp.json()['tree']
-
-    # Separate Blobs from Directories (Blobs contain only SHA's)
-    for item in file_tree:
-        if item['type'] == 'blob' and is_py_file(item['path']):
-            blob_urls[item['path']] = item['url']
-
-    # Get file contents for each Blob
-    payload = {'access_token': token}
-    headers = {'Accept': 'application/vnd.github.v3.raw'}
-    for url in blob_urls.values():
-        resp = requests.get(url, params=payload, headers=headers)
-        files.append(resp.text)
-
-    # Write Blob contents out to a python file
-    os.makedirs('tmp')
-    for index, text in enumerate(files):
-        with open('./tmp/{}.py'.format(index), 'w') as tmp_file:
-            tmp_file.write(text)
-
-    path = ['./tmp']
-    config = Config(
+    master_url = 'http://127.0.0.1:5000/'
+    cc_path = ['./tmp']
+    cc_config = Config(
         exclude='',
         ignore='venv',
         order=SCORE,
@@ -60,30 +22,69 @@ def work():
         max='F',
     )
 
-    h = CCHarvester(path, config)
-    # pprint(h._to_dicts())
-    print(calc_avg_cc(h._to_dicts()))
-    rmtree('tmp')
+    def __init__(self):
+        self.token = self.get_token()
 
+    def do_work(self):
+        # while True:
+        sha = self.get_work()
+        file_tree = self.get_file_tree(sha)
+        self.get_files(file_tree)
+        print('SHA: {}'.format(sha))
+        print(self.get_average_cc())
 
-def is_py_file(filename):
-    regex = '.*\.py'
-    return True if match(regex, filename) is not None else False
+    def get_token(self):
+        with open('github-token', 'r') as f:
+            return f.read().split()[0]
 
+    def get_work(self):
+        return requests.get(self.master_url).json()['sha']
 
-def calc_avg_cc(results):
-    total_cc = 0
-    for filename in results.values():
-        file_cc = 0
-        # print(filename)
-        for block in filename:
-            file_cc += block['complexity']
+    def get_file_tree(self, sha):
+        payload = {
+            'recursive': 'true',
+            'access_token': self.token
+        }
 
-        total_cc += file_cc
+        resp = requests.get(self.files_list_url.format(sha), params=payload)
+        if resp.status_code == 301:
+            redirect_url = resp.headers['location'].split('?')[0]
+            payload = {'access_token': self.token}
+            resp = requests.get(redirect_url, params=payload)
 
-    return total_cc / len(results)
+        return resp.json()['tree']
+
+    def get_files(self, file_tree):
+        os.makedirs('tmp')
+        blob_urls = []
+        for item in file_tree:
+            if item['type'] == 'blob' and self.is_py_file(item['path']):
+                blob_urls.append(item['url'])
+
+        payload = {'access_token': self.token}
+        headers = {'Accept': 'application/vnd.github.v3.raw'}
+        for index, url in enumerate(blob_urls):
+            resp = requests.get(url, params=payload, headers=headers)
+            with open('./tmp/{}.py'.format(index), 'w') as tmp_file:
+                tmp_file.write(resp.text)
+
+    def get_average_cc(self):
+        results = CCHarvester(self.cc_path, self.cc_config)._to_dicts()
+        total_cc = 0
+        for filename in results.values():
+            file_cc = 0
+            for block in filename:
+                file_cc += block['complexity']
+
+            total_cc += file_cc
+
+        rmtree('tmp')
+        return total_cc / len(results)
+
+    def is_py_file(self, filename):
+        return True if match('.*\.py', filename) is not None else False
 
 
 if __name__ == '__main__':
-    work()
-    print('Done work')
+    worker = Worker()
+    worker.do_work()
