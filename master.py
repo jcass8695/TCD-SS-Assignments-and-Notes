87e71cb53d331f5ffa6b65761ea4a0a518236f20
Jack Cassidy <j.cassidy45@gmail.com>
2017-11-29 17:58:04 +0000

    Add Master docstrings

--- master.py
+++ master.py
@@ -15,8 +15,15 @@ CC_LOCK = Lock()
 COMMIT_LIST_URL = 'https://api.github.com/repos/JCass45/CS4400-Internet-Applications-Repo-Complexity/commits'
 FILES_LIST_URL = 'https://api.github.com/repos/JCass45/CS4400-Internet-Applications-Repo-Complexity/git/trees/{}'
 
+TOTAL_COMMITS = 0
+
 
 class Master(Resource):
+    '''
+    Master issues Workers with commit SHA's for a given repo
+    and accepts the average CC of a commit from Workers
+    '''
+
     def get(self):
         with JOB_QUEUE_LOCK:
             try:
@@ -37,6 +44,8 @@ class Master(Resource):
 
 
 class NodeSetup(Resource):
+    ''' Issues newly instantiated Workers with a URL to access commits '''
+
     def get(self):
         print('New node joined')
         return {'url': FILES_LIST_URL}
@@ -44,7 +53,7 @@ class NodeSetup(Resource):
 
 def get_commits():
     '''
-    Retrieves SHA's for every commit in the CS4400 Chat Server repository by Jack Cassidy
+    Retrieves SHA's for every commit in the CS4400 Repo Complexity repository by Jack Cassidy,
     and fills the Job Queue
     '''
 
@@ -67,17 +76,13 @@ def get_commits():
 
 
 def calc_avg_cc():
-    '''
-    Calculate and print the average cyclomatic complexity for the repository
-    '''
+    ''' Calculate and print the average cyclomatic complexity for the repository '''
 
     print('-----AVG CC: {}-----'.format(sum(CC) / len(CC)))
 
 
 def shutdown_server():
-    '''
-    Snippet from flask-restful docs to shutdown a flask server
-    '''
+    ''' Snippet from flask-restful docs to shutdown a flask server '''
 
     func = request.environ.get('werkzeug.server.shutdown')
     if func is None:
@@ -90,7 +95,6 @@ api.add_resource(NodeSetup, '/init')
 
 if __name__ == '__main__':
     get_commits()
-    global TOTAL_COMMITS
     TOTAL_COMMITS = len(JOB_QUEUE)
     app.run(debug=False)
     print('\n-----Shutting Down Server-----')
