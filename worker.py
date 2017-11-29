83a36b9ee64c7889b3e09ac6c6ff3f33821200c2
Jack Cassidy <j.cassidy45@gmail.com>
2017-11-29 13:58:07 +0000

    Worker calculates average CC for a given commit
    Worker asks Master for a commit SHA. It downloads the file tree for the commit. For each Blob ('file') in the tree, separate the python and non python files. Get the file contents from Github for each python file and add it to a list. Create temp files and run Radon CC on each file and compute average. Clean up tmp dir afterwards

--- worker.py
+++ worker.py
@@ -1,7 +1,11 @@
+import os
+from shutil import rmtree
 from pprint import pprint
 from re import match
 import requests
-from radon.complexity import cc_visit
+from radon.complexity import cc_rank, SCORE
+from radon.cli.harvest import CCHarvester
+from radon.cli import Config
 
 
 def work():
@@ -11,12 +15,14 @@ def work():
 
     blob_urls = {}
     files = []
+    cc = 0
     sha = requests.get('http://127.0.0.1:5000/').json()['sha']
     payload = {
         'recursive': 'true',
         'access_token': token
     }
 
+    # Get file tree for given commit SHA
     resp = requests.get(files_list_url.format(sha), params=payload)
     if resp.status_code == 301:
         redirect_url = resp.headers['location'].split('?')[0]
@@ -24,20 +30,58 @@ def work():
         resp = requests.get(redirect_url, params=payload)
 
     file_tree = resp.json()['tree']
+
+    # Separate Blobs from Directories (Blobs contain only SHA's)
     for item in file_tree:
-        if item['type'] == 'blob' and is_code_file(item['path']):
+        if item['type'] == 'blob' and is_py_file(item['path']):
             blob_urls[item['path']] = item['url']
 
+    # Get file contents for each Blob
     payload = {'access_token': token}
     headers = {'Accept': 'application/vnd.github.v3.raw'}
     for url in blob_urls.values():
         resp = requests.get(url, params=payload, headers=headers)
         files.append(resp.text)
 
+    # Write Blob contents out to a python file
+    os.makedirs('tmp')
+    for index, text in enumerate(files):
+        with open('./tmp/{}.py'.format(index), 'w') as tmp_file:
+            tmp_file.write(text)
 
-def is_code_file(filename):
-    regex = '.*\.py|.*\.sh|.*\.c|.*\.cpp|.*\.h|.*\.txt|.*\.hs|.*\.rb|.*\.java|.*\.class|.*\.js|.*\.css|.*\.html'
+    path = ['./tmp']
+    config = Config(
+        exclude='',
+        ignore='venv',
+        order=SCORE,
+        no_assert=True,
+        show_closures=False,
+        min='A',
+        max='F',
+    )
+
+    h = CCHarvester(path, config)
+    # pprint(h._to_dicts())
+    print(calc_avg_cc(h._to_dicts()))
+    rmtree('tmp')
+
+
+def is_py_file(filename):
+    regex = '.*\.py'
     return True if match(regex, filename) is not None else False
+
+
+def calc_avg_cc(results):
+    total_cc = 0
+    for filename in results.values():
+        file_cc = 0
+        # print(filename)
+        for block in filename:
+            file_cc += block['complexity']
+
+        total_cc += file_cc
+
+    return total_cc / len(results)
 
 
 if __name__ == '__main__':
