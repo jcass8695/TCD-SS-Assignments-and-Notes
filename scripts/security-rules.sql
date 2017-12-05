a95e15ab8504cf1014d2aa869e83407fff0a8537
Jack Cassidy <j.cassidy45@gmail.com>
2017-12-05 18:54:56 +0000

    clean file structure

---
+++ scripts/security-rules.sql
@@ -0,0 +1,5 @@
+grant select on SuperPrivacy to PoliceForce;
+grant select, insert, update on Disaster to PoliceForce;
+grant select, insert, update on HeroDisaster to PoliceForce;
+grant select, insert, update on VillianDisaster to PoliceForce;
+grant insert, update on Equipment to LuciusFox;
