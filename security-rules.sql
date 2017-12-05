02428f078286fceca86308a3d87105a51958e5af
Jack Cassidy <j.cassidy45@gmail.com>
2017-12-05 18:51:00 +0000

    Clean file structure

--- security-rules.sql
+++
@@ -1,5 +0,0 @@
-grant select on SuperPrivacy to PoliceForce;
-grant select, insert, update on Disaster to PoliceForce;
-grant select, insert, update on HeroDisaster to PoliceForce;
-grant select, insert, update on VillianDisaster to PoliceForce;
-grant insert, update on Equipment to LuciusFox;
--- update-hideout.sql
+++
@@ -1,4 +0,0 @@
------ Move all Marvel heroes to Avenger's Tower -----
-update SuperPerson
-set HideoutID = 8
-where Universe = 'Marvel' and Disposition = 'GOOD';
